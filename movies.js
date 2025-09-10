469fd6a613e7ec4cc4181c65955d772a646f75bd
GregularJoe <gjlarson.pa@gmail.com>
2025-09-10 19:15:48 -0400

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,7 @@
+// movies.js
+const movies = [
+  "Star Wars: A New Hope (1977)",
+  "Star Wars: The Empire Strikes Back (1980)",
+  "Star Wars: Return of the Jedi (1983)",
+  // ...all other movies
+];
